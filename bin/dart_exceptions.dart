7b9a77d746416733dfe9726e6be4af59be31b670
Franklin Almeida Campos <franklindrw@gmail.com>
2022-12-31 22:48:28 -0300

    adicionando exceptions controller

--- bin/dart_exceptions.dart
+++ bin/dart_exceptions.dart
@@ -23,3 +23,23 @@ void main() {
   // Observando resultado
   print(result);
 }
+
+// void main() {
+//   print("Started Main");
+//   functionOne();
+//   print("Finished Main");
+// }
+
+// void functionOne() {
+//   print("Started F01");
+//   functionTwo();
+//   print("Finished F01");
+// }
+
+// void functionTwo() {
+//   print("Started F02");
+//   for (int i = 1; i <= 5; i++) {
+//     print(i);
+//   }
+//   print("Finished F02");
+// }
